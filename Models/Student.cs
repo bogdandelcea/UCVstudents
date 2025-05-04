ea05ed2aa1c85c82c4b6ff3520a534771f16e9b0
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-04 23:23:21 +0300

    add Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UCVstudents.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string RegistrationNumber { get; set; } = string.Empty;
+
+        public DateTime EnrollmentDate { get; set; } = DateTime.Now;
+    }
+}
