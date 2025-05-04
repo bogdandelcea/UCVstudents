331fe1f91de3c342a7b04342714b94fb96bc4dc7
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-05 00:16:41 +0300

    add IStudentService.cs

---
+++ Serivices/Interfaces/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UCVstudents.Models;
+
+namespace UCVstudents.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<Student>> GetAllStudentsAsync();
+    }
+}
