57156c7f34db33cfa67e90cb708bf13454ceb608
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-04 22:54:07 +0300

    add Repositories/Interfaces (#27)

---
+++ Repositories/Interfaces/IRepositoryWrapper.cs
@@ -0,0 +1,9 @@
+﻿namespace UCVstudents.Repositories.Interfaces
+{
+    public interface IRepositoryWrapper
+    {
+        IStudentRepository Student { get; }
+        // Adaugă și altele ulterior (ex: ITeacherRepository, IUserRepository etc.)
+        void Save();
+    }
+}
