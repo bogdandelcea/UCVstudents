c4e2206ad5204f5c3bf78d3a0f6b5bf66d49f7e1
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-05 00:30:33 +0300

    Create StudentService.cs

---
+++ Services/StudentService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UCVstudents.Models;
+using UCVstudents.Repositories.Interfaces;
+using UCVstudents.Services.Interfaces;
+
+namespace UCVstudents.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IRepositoryWrapper _repository;
+
+        public StudentService(IRepositoryWrapper repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Student>> GetAllStudentsAsync()
+        {
+            // Va fi async când implementăm repo-ul
+            return await Task.FromResult(_repository.Student.GetAll());
+        }
+    }
+}
