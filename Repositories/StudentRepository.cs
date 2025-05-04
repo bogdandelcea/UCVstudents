a679feb8686042efd5dd42e507bb2e7bc2c7030f
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-04 23:06:15 +0300

    add Wrapper and Student

---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using UCVstudents.Data;
+using UCVstudents.Repositories.Interfaces;
+
+namespace UCVstudents.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Vom adăuga metode în pasul următor (ex: GetAll, GetById etc.)
+    }
+}
