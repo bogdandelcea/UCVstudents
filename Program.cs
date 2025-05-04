7fe006a7d781e1c953205b131e8e6e3d82fc5cbe
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-04 15:23:34 +0300

    add Program.cs and .csproj

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using UCVstudents.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddDefaultTokenProviders()
+    .AddDefaultUI()
+    .AddEntityFrameworkStores<ApplicationDbContext>(); // asigură-te că ai și EF configurat
+
+
+
 var app = builder.Build();
+
+app.MapRazorPages();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,6 +41,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
 app.MapStaticAssets();
 
 app.MapControllerRoute(
@@ -26,4 +50,7 @@ app.MapControllerRoute(
     .WithStaticAssets();
 
 
+
 app.Run();
+
+app.MapRazorPages();
