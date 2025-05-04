70bcba38538d3c50c4b09f6e0f9f70a6fc29aec3
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-04 23:42:34 +0300

    update (#36)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UCVstudents.Models;
 
 namespace UCVstudents.Data
 {
@@ -12,7 +13,7 @@ namespace UCVstudents.Data
         }
 
         // Exemplu de entități (activează doar dacă ai creat modelele)
-        // public DbSet<Student> Students { get; set; }
+        public DbSet<Student> Students { get; set; }
         // public DbSet<Teacher> Teachers { get; set; }
         // public DbSet<Subject> Subjects { get; set; }
         // public DbSet<Grade> Grades { get; set; }
