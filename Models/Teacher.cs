d07c145523535d356719cd10002b908985fdca33
Ursu Mihai <147542508+ursu-mihai77777@users.noreply.github.com>
2025-05-05 20:42:39 +0300

    Update Teacher.cs (#54)

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -1,6 +1,23 @@
-﻿namespace UCVstudents.Models
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UCVstudents.Models
 {
     public class Teacher
     {
+        [Key]
+        public int TeacherId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Sex { get; set; }
+        public string? CNP { get; set; }
+        public int? Age { get; set; }
+        public string? PhoneNr { get; set; }
+        public string? Role { get; set; }
+        public string? Faculty { get; set; }
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
     }
 }
