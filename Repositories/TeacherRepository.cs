4bd06de69b0e9dd2b15256fb9b35bb9d65e667ce
Ursu Mihai <147542508+ursu-mihai77777@users.noreply.github.com>
2025-05-05 20:36:18 +0300

    Add files via upload

---
+++ Repositories/TeacherRepository.cs
@@ -0,0 +1,14 @@
+﻿using UCVStudents.Data;
+using UCVStudents.Models;
+using UCVStudents.Repositories.Interfaces;
+
+namespace UCVStudents.Repositories
+{
+    public class TeacherRepository : RepositoryBase<Teacher>, ITeacherRepository
+    {
+        public TeacherRepository(ApplicationDbContext applicationDbContext)
+            : base(applicationDbContext)
+        {
+        }
+    }
+}
