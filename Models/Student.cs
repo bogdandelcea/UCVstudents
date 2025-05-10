e09f4d9d4090ef4803ebc896712a6bad0fcf5227
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-11 02:51:10 +0300

    Update Student.cs (#101)

--- Models/Student.cs
+++ Models/Student.cs
@@ -7,11 +7,9 @@ namespace UCVstudents.Models
         public int Id { get; set; }
 
         [Required]
-        [Display(Name = "First Name")]
         public string FirstName { get; set; } = string.Empty;
 
         [Required]
-        [Display(Name = "Last Name")]
         public string LastName { get; set; } = string.Empty;
 
         [Required]
