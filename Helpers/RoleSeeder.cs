fd216aff1c9a54bf487d0b1d00d26ef3a7959e37
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-10 18:22:07 +0300

    Create RoleSeeder.cs

--- Helpers/RoleSeeder.cs
+++ Helpers/RoleSeeder.cs
@@ -1,6 +1,20 @@
-﻿namespace UCVstudents.Helpers
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UCVstudents.Helpers
 {
-    public class RoleSeeder
+    public static class RoleSeeder
     {
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roleNames = { "Admin", "Student", "Teacher" };
+
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
     }
 }
