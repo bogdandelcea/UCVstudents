d2e71ee4838cd13ace71bb2b5ce346bf298c72e3
Bogdan-Alexandru Delcea <delcea.bogdan2003@gmail.com>
2025-05-11 03:21:29 +0300

    Adaugare fisiere pentru Student

--- Repositories/Interfaces/IStudentRepository.cs
+++ Repositories/Interfaces/IStudentRepository.cs
@@ -1,9 +1,15 @@
-﻿using UCVstudents.Models;
+﻿using System.Collections.Generic;
+using UCVstudents.Models;
 
 namespace UCVstudents.Repositories.Interfaces
 {
     public interface IStudentRepository
     {
         IEnumerable<Student> GetAll();
+        Student GetById(int id);
+        void Create(Student student);
+        void Update(Student student);
+        void Delete(Student student);
+        void Save();
     }
 }
