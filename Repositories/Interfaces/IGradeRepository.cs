8866a6b5cff78fcdf7554fbf507278b8277f22c2
Ursu Mihai <147542508+ursu-mihai77777@users.noreply.github.com>
2025-05-11 21:58:32 +0300

    Update IGradeRepository.cs (#107)

--- Repositories/Interfaces/IGradeRepository.cs
+++ Repositories/Interfaces/IGradeRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace UCVstudents.Repositories.Interfaces
+using System.Collections.Generic;
+using UCVstudents.Models;
+
+namespace UCVstudents.Repositories.Interfaces
 {
     public interface IGradeRepository
     {
+        IEnumerable<Grade> GetAll();
+        Grade GetById(int id);
+        void Create(Grade grade);
+        void Update(Grade grade);
+        void Delete(Grade grade);
+        void Save();
     }
 }
