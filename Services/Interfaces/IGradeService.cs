19914f89a682481320df31c0cbf1b92f5f39b77a
Ursu Mihai <147542508+ursu-mihai77777@users.noreply.github.com>
2025-05-11 22:21:29 +0300

    Update IGradeService.cs

--- Services/Interfaces/IGradeService.cs
+++ Services/Interfaces/IGradeService.cs
@@ -1,6 +1,14 @@
-﻿namespace UCVstudents.Services.Interfaces
+using System.Collections.Generic;
+using UCVstudents.Models;
+
+namespace UCVstudents.Services.Interfaces
 {
     public interface IGradeService
     {
+        IEnumerable<Grade> GetAll();
+        Grade GetById(int id);
+        void Create(Grade grade);
+        void Update(Grade grade);
+        void Delete(int id);
     }
 }
