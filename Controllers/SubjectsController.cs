7065340fca98ac219f053bf8459ea7425683c3e5
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-11 16:10:58 +0300

    Modificare DeleteConfirmed -> Delete (#105)

--- Controllers/SubjectsController.cs
+++ Controllers/SubjectsController.cs
@@ -60,7 +60,8 @@ namespace UCVstudents.Controllers
             return View(subject);
         }
 
-        [HttpPost, ActionName("DeleteConfirmed")]
+        [HttpPost, ActionName("Delete")]
+
         public IActionResult DeleteConfirmed(int id)
         {
             _subjectService.Delete(id);
