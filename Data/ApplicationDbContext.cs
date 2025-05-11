16b540aaf73a9801dbe0d7e0466950f2ed35abed
Ursu Mihai <147542508+ursu-mihai77777@users.noreply.github.com>
2025-05-11 23:00:22 +0300

    Update ApplicationDbContext.cs (#119)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -17,6 +17,6 @@ namespace UCVstudents.Data
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Subject> Subjects { get; set; }
         public DbSet<Grade> Grades { get; set; }
-        // public DbSet<Document> Documents { get; set; }
+        public DbSet<Document> Documents { get; set; }
     }
 }
