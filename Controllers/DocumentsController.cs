fc7e39eae1f6c9483c43398862b9f1a4130f9267
Ursu Mihai <147542508+ursu-mihai77777@users.noreply.github.com>
2025-05-12 16:16:23 +0300

    Update DocumentsController.cs

--- Controllers/DocumentsController.cs
+++ Controllers/DocumentsController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using UCVstudents.Models;
+using UCVstudents.Services.Interfaces;
 
 namespace UCVstudents.Controllers
 {
     public class DocumentsController : Controller
     {
-        public IActionResult Index()
+        private readonly IDocumentsService _documentService;
+
+        public DocumentsController(IDocumentService documentService)
         {
-            return View();
+            _documentService = documentService;
+        }
+
+        public IActionResult Index() => View(_documentService.GetAll());
+
+        public IActionResult Details(int id)
+        {
+            var document = _documentService.GetById(id);
+            if (document == null) return NotFound();
+            return View(document);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                _documentService.Create(document);
+                return RedirectToAction("Index");
+            }
+            return View(document);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var document = _documentService.GetById(id);
+            if (document == null) return NotFound();
+            return View(document);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                _documentService.Update(document);
+                return RedirectToAction("Index");
+            }
+            return View(document);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var document = _documentService.GetById(id);
+            if (document == null) return NotFound();
+            return View(document);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _documentService.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
