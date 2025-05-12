36f48c867d67a05959db2bb848fe4838e6f447bc
Ursu Mihai <147542508+ursu-mihai77777@users.noreply.github.com>
2025-05-12 15:56:06 +0300

    Update GradesController.cs

--- Controllers/GradesController.cs
+++ Controllers/GradesController.cs
@@ -6,136 +6,73 @@ using Microsoft.EntityFrameworkCore;
 using UCVstudents.Data;
 using UCVstudents.Models;
 
+using Microsoft.AspNetCore.Mvc;
+using UCVstudents.Models;
+using UCVstudents.Services.Interfaces;
+
 namespace UCVstudents.Controllers
 {
-    public class GradesController : Controller
+    public class GradeController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IGradeService _gradeService;
 
-        public GradesController(ApplicationDbContext context)
+        public GradeController(IGradeService gradeService)
         {
-            _context = context;
+            _gradeService = gradeService;
         }
 
-        // GET: Grade
-        public async Task<IActionResult> Index()
-        {
-            var grades = _context.Grades
-                                 .Include(g => g.Student)
-                                 .Include(g => g.Subject);
-            return View(await grades.ToListAsync());
-        }
+        public IActionResult Index() => View(_gradeService.GetAll());
 
-        // GET: Grade/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public IActionResult Details(int id)
         {
-            if (id == null) return NotFound();
-
-            var grade = await _context.Grades
-                                      .Include(g => g.Student)
-                                      .Include(g => g.Subject)
-                                      .FirstOrDefaultAsync(m => m.GradeId == id);
+            var grade = _gradeService.GetById(id);
             if (grade == null) return NotFound();
-
             return View(grade);
         }
 
-        // GET: Grade/Create
-        public IActionResult Create()
-        {
-            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "FullName");
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "SubjectId", "Name");
-            return View();
-        }
+        public IActionResult Create() => View();
 
-        // POST: Grade/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("GradeId,StudentId,SubjectId,Value,DateReceived")] Grade grade)
+        public IActionResult Create(Grade grade)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(grade);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                _gradeService.Create(grade);
+                return RedirectToAction("Index");
             }
-            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "FullName", grade.StudentId);
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "SubjectId", "Name", grade.SubjectId);
             return View(grade);
         }
 
-        // GET: Grade/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public IActionResult Edit(int id)
         {
-            if (id == null) return NotFound();
-
-            var grade = await _context.Grades.FindAsync(id);
+            var grade = _gradeService.GetById(id);
             if (grade == null) return NotFound();
-
-            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "FullName", grade.StudentId);
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "SubjectId", "Name", grade.SubjectId);
             return View(grade);
         }
 
-        // POST: Grade/Edit/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("GradeId,StudentId,SubjectId,Value,DateReceived")] Grade grade)
+        public IActionResult Edit(Grade grade)
         {
-            if (id != grade.GradeId) return NotFound();
-
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(grade);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GradeExists(grade.GradeId))
-                        return NotFound();
-                    else
-                        throw;
-                }
-                return RedirectToAction(nameof(Index));
+                _gradeService.Update(grade);
+                return RedirectToAction("Index");
             }
-            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "FullName", grade.StudentId);
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "SubjectId", "Name", grade.SubjectId);
             return View(grade);
         }
 
-        // GET: Grade/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public IActionResult Delete(int id)
         {
-            if (id == null) return NotFound();
-
-            var grade = await _context.Grades
-                                      .Include(g => g.Student)
-                                      .Include(g => g.Subject)
-                                      .FirstOrDefaultAsync(m => m.GradeId == id);
+            var grade = _gradeService.GetById(id);
             if (grade == null) return NotFound();
-
             return View(grade);
         }
 
-        // POST: Grade/Delete/5
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public IActionResult DeleteConfirmed(int id)
         {
-            var grade = await _context.Grades.FindAsync(id);
-            if (grade != null)
-            {
-                _context.Grades.Remove(grade);
-                await _context.SaveChangesAsync();
-            }
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool GradeExists(int id)
-        {
-            return _context.Grades.Any(e => e.GradeId == id);
+            _gradeService.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
