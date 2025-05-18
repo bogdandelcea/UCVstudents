8246aa8c8ef507159e744f23539ab291f607a44f
Bogdan-Alexandru Delcea <148143648+bogdandelcea@users.noreply.github.com>
2025-05-18 15:59:13 +0300

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,10 +13,10 @@ namespace UCVstudents.Data
         }
 
         // Exemplu de entități (activează doar dacă ai creat modelele)
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Teacher> Teachers { get; set; }
-        public DbSet<Subject> Subjects { get; set; }
-        public DbSet<Grade> Grades { get; set; }
-        public DbSet<Document> Documents { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Teacher> Teacher { get; set; }
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<Grade> Grade { get; set; }
+        public DbSet<Document> Document { get; set; }
     }
 }
